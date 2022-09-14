a661df1574857b4a8797926885cb368096f6baaf
PhamPhuHoang <phamphuhoang245@gmail.com>
2022-09-14 22:40:18 +0700

    add Error

---
+++ FrontendNews/src/Pages/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            Bạn không có quyền vào trang này{' '}
+            <a href="http://localhost:3000/loginadmin">Về trang đăng nhập</a>
+        </div>
+    );
+};
+
+export default Error;
